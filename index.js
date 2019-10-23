8c3063230feb1a0dea477e21095fcda1f917a849
flyingswan3000 <flyingswan3000@gmail.com>
2019-10-23 20:36:10 +0800

    2 files modified, 2 added

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.css';
+
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
 import Root from './components/Root'
+import reducer from './reducer'
+import mainSaga from './saga'
+import './style.css';
+
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware()
+// mount it on the Store
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware)
+)
+
+// then run the saga
+sagaMiddleware.run(mainSaga)
 
 ReactDOM.render(<Root />, document.getElementById("root"));
 
